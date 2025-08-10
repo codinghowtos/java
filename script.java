1e63f37c7e3506def30a4aa54f622ddf50a565c3
codinghowtos <coadinghowtos@gmail.com>
2025-08-10 22:50:28 +0530

    envtesting

--- script.java
+++ script.java
@@ -1,5 +1,5 @@
-class demotesting{
+public class script{
     public static void main(String[] args) {
-        System.out.println("codinghowtos");
+        System.out.println("Hello, world!");
     }
-};
+}
