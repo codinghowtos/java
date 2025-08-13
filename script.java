57b4d87c94198b7a0cef9a39a3c2eadfb43b9a59
codinghowtos <coadinghowtos@gmail.com>
2025-08-13 18:40:42 +0530

    hello world

--- script.java
+++ script.java
@@ -1,5 +1,11 @@
+
+import java.util.*;
 public class script {
     public static void main(String[] args) {
-       System.out.println("hello world"); 
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        name  = "jack";
+        System.out.println(name);
     }
 }
