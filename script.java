394bb849e5c94a144bfec8ad3a023b29a5dcae40
codinghowtos <coadinghowtos@gmail.com>
2025-08-21 09:51:11 +0530

    loops in java

--- script.java
+++ script.java
@@ -2,18 +2,12 @@
 import java.util.*;
 public class script {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String colour = sc.next();
-      switch (colour) {
-        case "Red":
-            System.out.println("colour is red");
-            break;
-        case "Green":
-        System.out.println("colour is green");
-        break;
-        default:
-        System.out.println("choose a colour");
-      }
-       
+      Scanner sc = new Scanner(System.in);
+//int num = sc.nextInt();
+int i = 1;
+do {
+  System.out.println(i);
+  i++;
+} while (i==0);
     }
 }
